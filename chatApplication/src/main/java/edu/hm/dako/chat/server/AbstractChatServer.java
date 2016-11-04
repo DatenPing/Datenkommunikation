f287b87f80c4269068635c5d65a9f0c40fbb363b
cbmk5 <ckoenigs@hm.edu>
2016-11-04 10:00:12 +0100

    1. ChatServerImpl parametrisiert 2. Formatting

--- chatApplication/src/main/java/edu/hm/dako/chat/server/AbstractChatServer.java
+++ chatApplication/src/main/java/edu/hm/dako/chat/server/AbstractChatServer.java
@@ -1,21 +1,29 @@
 package edu.hm.dako.chat.server;
 
+import edu.hm.dako.chat.connection.Connection;
+
 /**
  * Gemeinsame Attribute fuer alle Implementierungen
- * 
- * @author Peter Mandl
  *
+ * @author Peter Mandl
  */
 public abstract class AbstractChatServer implements ChatServerInterface {
 
-	// Gemeinsam fuer alle Workerthreads verwaltete Liste aller eingeloggten
-	// Clients
-	protected SharedChatClientList clients;
+    // Gemeinsam fuer alle Workerthreads verwaltete Liste aller eingeloggten
+    // Clients
+    protected SharedChatClientList clients;
 
-	// Zaehler fuer Test
-	protected SharedServerCounter counter;
+    // Zaehler fuer Test
+    protected SharedServerCounter counter;
 
-	// Referenz auf Server GUI fuer die Meldung von Ereignissen
-	protected ChatServerGuiInterface serverGuiInterface;
+    // Referenz auf Server GUI fuer die Meldung von Ereignissen
+    protected ChatServerGuiInterface serverGuiInterface;
 
+    /**
+     * Erzeugt den zur Bearbeitung der Anfrage notwendigen WorkerThread.
+     *
+     * @param connection die für den workerThread notwendigeb Verbindungsinformationen
+     * @return einen neuen WorkerThread zur Bearbeitung
+     */
+    protected abstract AbstractWorkerThread getWorkerThreadImpl(Connection connection);
 }
--- chatApplication/src/main/java/edu/hm/dako/chat/server/SimpleChatServerImpl.java
+++ chatApplication/src/main/java/edu/hm/dako/chat/server/SimpleChatServerImpl.java
@@ -1,17 +1,16 @@
 package edu.hm.dako.chat.server;
 
-import java.util.Vector;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import edu.hm.dako.chat.common.ClientListEntry;
 import edu.hm.dako.chat.common.ExceptionHandler;
 import edu.hm.dako.chat.connection.Connection;
 import edu.hm.dako.chat.connection.ServerSocketInterface;
 import javafx.concurrent.Task;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * <p/>
@@ -21,103 +20,106 @@ import javafx.concurrent.Task;
  */
 public class SimpleChatServerImpl extends AbstractChatServer {
 
-	private static Log log = LogFactory.getLog(SimpleChatServerImpl.class);
+    private static Log log = LogFactory.getLog(SimpleChatServerImpl.class);
 
-	// Threadpool fuer Worker-Threads
-	private final ExecutorService executorService;
+    // Threadpool fuer Worker-Threads
+    private final ExecutorService executorService;
 
-	// Socket fuer den Listener, der alle Verbindungsaufbauwuensche der Clients
-	// entgegennimmt
-	private ServerSocketInterface socket;
+    // Socket fuer den Listener, der alle Verbindungsaufbauwuensche der Clients
+    // entgegennimmt
+    private ServerSocketInterface socket;
 
-	/**
-	 * Konstruktor
-	 * 
-	 * @param executorService
-	 * @param socket
-	 * @param serverGuiInterface
-	 */
-	public SimpleChatServerImpl(ExecutorService executorService,
-			ServerSocketInterface socket, ChatServerGuiInterface serverGuiInterface) {
-		log.debug("SimpleChatServerImpl konstruiert");
-		this.executorService = executorService;
-		this.socket = socket;
-		this.serverGuiInterface = serverGuiInterface;
-		counter = new SharedServerCounter();
-		counter.logoutCounter = new AtomicInteger(0);
-		counter.eventCounter = new AtomicInteger(0);
-		counter.confirmCounter = new AtomicInteger(0);
-	}
+    /**
+     * Konstruktor
+     *
+     * @param executorService
+     * @param socket
+     * @param serverGuiInterface
+     */
+    public SimpleChatServerImpl(ExecutorService executorService,
+                                ServerSocketInterface socket, ChatServerGuiInterface serverGuiInterface) {
+        log.debug("SimpleChatServerImpl konstruiert");
+        this.executorService = executorService;
+        this.socket = socket;
+        this.serverGuiInterface = serverGuiInterface;
+        counter = new SharedServerCounter();
+        counter.logoutCounter = new AtomicInteger(0);
+        counter.eventCounter = new AtomicInteger(0);
+        counter.confirmCounter = new AtomicInteger(0);
+    }
 
 
+    @Override
+    public void start() {
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                // Clientliste erzeugen
+                clients = SharedChatClientList.getInstance();
 
-	@Override
-	public void start() {
-		Task<Void> task = new Task<Void>() {
-			@Override
-			protected Void call() throws Exception {
-				// Clientliste erzeugen
-				clients = SharedChatClientList.getInstance();
+                while (!Thread.currentThread().isInterrupted() && !socket.isClosed()) {
+                    try {
+                        // Auf ankommende Verbindungsaufbauwuensche warten
+                        System.out.println(
+                                "SimpleChatServer wartet auf Verbindungsanfragen von Clients...");
 
-				while (!Thread.currentThread().isInterrupted() && !socket.isClosed()) {
-					try {
-						// Auf ankommende Verbindungsaufbauwuensche warten
-						System.out.println(
-								"SimpleChatServer wartet auf Verbindungsanfragen von Clients...");
+                        Connection connection = socket.accept();
+                        log.debug("Neuer Verbindungsaufbauwunsch empfangen");
 
-						Connection connection = socket.accept();
-						log.debug("Neuer Verbindungsaufbauwunsch empfangen");
+                        // Neuen Workerthread starten
+                        executorService.submit(getWorkerThreadImpl(connection));
+                    } catch (Exception e) {
+                        if (socket.isClosed()) {
+                            log.debug("Socket wurde geschlossen");
+                        } else {
+                            log.error(
+                                    "Exception beim Entgegennehmen von Verbindungsaufbauwuenschen: " + e);
+                            ExceptionHandler.logException(e);
+                        }
+                    }
+                }
+                return null;
+            }
+        };
 
-						// Neuen Workerthread starten
-						executorService.submit(new SimpleChatWorkerThreadImpl(connection, clients,
-								counter, serverGuiInterface));
-					} catch (Exception e) {
-						if (socket.isClosed()) {
-							log.debug("Socket wurde geschlossen");
-						} else {
-							log.error(
-									"Exception beim Entgegennehmen von Verbindungsaufbauwuenschen: " + e);
-							ExceptionHandler.logException(e);
-						}
-					}
-				}
-				return null;
-			}
-		};
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
 
-		Thread th = new Thread(task);
-		th.setDaemon(true);
-		th.start();
+    }
 
-	}
+    @Override
+    protected AbstractWorkerThread getWorkerThreadImpl(Connection connection) {
+        return new SimpleChatWorkerThreadImpl(connection, clients, counter, serverGuiInterface);
+    }
 
-	@Override
-	public void stop() throws Exception {
+    @Override
+    public void stop() throws Exception {
 
-		// Alle Verbindungen zu aktiven Clients abbauen
-		Vector<String> sendList = clients.getClientNameList();
-		for (String s : new Vector<String>(sendList)) {
-			ClientListEntry client = clients.getClient(s);
-			try {
-				if (client != null) {
-					client.getConnection().close();
-					log.error("Verbindung zu Client " + client.getUserName() + " geschlossen");
-				}
-			} catch (Exception e) {
-				log.debug(
-						"Fehler beim Schliessen der Verbindung zu Client " + client.getUserName());
-				ExceptionHandler.logException(e);
-			}
-		}
+        // Alle Verbindungen zu aktiven Clients abbauen
+        Vector<String> sendList = clients.getClientNameList();
+        for (String s : new Vector<String>(sendList)) {
+            ClientListEntry client = clients.getClient(s);
+            try {
+                if (client != null) {
+                    client.getConnection().close();
+                    log.error("Verbindung zu Client " + client.getUserName() + " geschlossen");
+                }
+            } catch (Exception e) {
+                log.debug(
+                        "Fehler beim Schliessen der Verbindung zu Client " + client.getUserName());
+                ExceptionHandler.logException(e);
+            }
+        }
 
-		// Loeschen der Userliste
-		clients.deleteAll();
-		Thread.currentThread().interrupt();
-		socket.close();
-		log.debug("Listen-Socket geschlossen");
-		executorService.shutdown();
-		log.debug("Threadpool freigegeben");
+        // Loeschen der Userliste
+        clients.deleteAll();
+        Thread.currentThread().interrupt();
+        socket.close();
+        log.debug("Listen-Socket geschlossen");
+        executorService.shutdown();
+        log.debug("Threadpool freigegeben");
 
-		System.out.println("SimpleChatServer beendet sich");
-	}
+        System.out.println("SimpleChatServer beendet sich");
+    }
 }
